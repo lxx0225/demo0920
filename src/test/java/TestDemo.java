5f1d9683cecaed6531e0e1c9b161bca94293851b
lxx <709990565@qq.com>
2019-10-06 14:10:08 +0800

    init

---
+++ src/test/java/TestDemo.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestDemo {
+    @Test
+    public void testDemo1(){
+        assertTrue(true);
+
+    }
+    @Test
+    public void testDemo2(){
+        assertTrue(false);
+    }
+
+}
